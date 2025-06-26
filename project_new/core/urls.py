334f3368cddda141d92465ad286aedb3a1cdef85
R.M.Bineth Geenuka <143432257+Geenukabineth@users.noreply.github.com>
2025-06-26 21:30:08 +0530

    Update urls.py

--- project_new/core/urls.py
+++ project_new/core/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views 
 
 
-
 urlpatterns = [
-    path("", views.landing),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.signup_view, name='register'),
-    path("base/", views.base, name="base"),
-    path("adminview/", views.adminview, name="adminview"),
-    path("create_product/", views.create_product, name="create_product"),
+    path("", views.landing, name="landing"),
+    path("login/", views.login_view, name="login"),
+    path("register/", views.signup_view, name="register"),
+    path("base/", views.dashboard_view, name="base"),
+    path("adminview/", views.admin_panel_view, name="adminview"),
+    path("create-product/", views.create_product, name="create_product"),
     path("logout/", views.logout_view, name="logout"),
     path("user_data/", views.user_data, name="user_data"),
     path("create_event/", views.create_event, name="create_event"),
